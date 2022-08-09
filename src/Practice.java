3f40d1d42b7af7df2b555456c643fe99df44a11f
Oleksii <jawuk.alex@gmail.com>
2022-08-09 18:28:19 +0300

    do, while

--- src/Practice.java
+++ src/Practice.java
@@ -6,15 +6,15 @@ public class Practice {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("You are playing a game! Press q or Q to quit");
-        String response = scanner.next();
+        String name = "";
 
-        if(response.equals("q") || response.equals("Q")) {
-            System.out.println("You quit the game");
-        }
-        else {
-            System.out.println("You are still playing the game *pew pew*");
+        do {
+            System.out.println("Enter your name");
+            name = scanner.nextLine();
+        }while(name.isBlank());
+            System.out.println("Hello " + name);
         }
     }
 
-}
+
 
