34befe44edb8444d40f9e371126d615755c0294f
Oleksii Iashchuk <jawuk.alex@gmail.com>
2022-08-13 00:32:07 +0300

    printf 2

--- src/Practice.java
+++ src/Practice.java
@@ -15,12 +15,15 @@ public class Practice {
         int myInt = 50;
         double myDouble = 1000;
 
-        System.out.printf("%b", myBoolean);
-        System.out.printf("%c", myChar);
-        System.out.printf("%s",myString);
-        System.out.printf("%d",myInt);
-        System.out.printf("%f", myDouble);
+        //System.out.printf("%b", myBoolean);
+        //System.out.printf("%c", myChar);
+        //System.out.printf("%s",myString);
+        //System.out.printf("%d",myInt);
+        //System.out.printf("%f", myDouble);
 
+        System.out.printf("You have this much money %.2f", myDouble);
+        System.out.println();
+        System.out.printf("You have this much money %,f", myDouble);
 
 
     }
