8fceda16dcec8ea86067bb168617fb1649aa73e4
Oleksii <jawuk.alex@gmail.com>
2022-08-12 15:58:52 +0300

    overloaded method

--- src/Practice.java
+++ src/Practice.java
@@ -8,33 +8,36 @@ import java.util.*;
 public class Practice {
     public static void main(String args[]) {
 
-        ArrayList<ArrayList<String>> groceryList = new ArrayList<>();
 
-        ArrayList<String> bakeryList = new ArrayList();
-        bakeryList.add("pasta");
-        bakeryList.add("bread");
-        bakeryList.add("donuts");
+        double x = add(1.1,2.2, 3.3, 4.4);
 
-        ArrayList<String> produceList = new ArrayList();
-        produceList.add("tomato");
-        produceList.add("onion");
-        produceList.add("beans");
+        System.out.println(x);
 
-        ArrayList<String> drinkList = new ArrayList();
-        drinkList.add("cola");
-        drinkList.add("gin");
-        drinkList.add("vodka");
+    }
 
-        groceryList.add(bakeryList);
-        groceryList.add(produceList);
-        groceryList.add(drinkList);
-
-        System.out.println(groceryList.get(2).get(2));
-
-
-
-
-
+    static int add(int a, int b){
+        System.out.println("This is overloaded method #1");
+        return a+b;
+    }
+    static int add(int a, int b, int c ){
+        System.out.println("This is overloaded method #2");
+        return a+b+c;
+    }
+    static int add(int a, int b, int c, int d){
+        System.out.println("This is overloaded method #3");
+        return a+b+c+d;
+    }
+    static double add(double a, double b){
+        System.out.println("This is overloaded method #4");
+        return a+b;
+    }
+    static double add(double a, double b, double c ){
+        System.out.println("This is overloaded method #5");
+        return a+b+c;
+    }
+    static double add(double a, double b, double c, double d){
+        System.out.println("This is overloaded method #6");
+        return a+b+c+d;
     }
 }
 
