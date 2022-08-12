be2ad58426dfaeecae976da57d97946d37ae9c79
Oleksii <jawuk.alex@gmail.com>
2022-08-12 18:38:07 +0300

    printf

--- src/Practice.java
+++ src/Practice.java
@@ -9,35 +9,20 @@ public class Practice {
     public static void main(String args[]) {
 
 
-        double x = add(1.1,2.2, 3.3, 4.4);
+        boolean myBoolean = true;
+        char myChar = '@';
+        String myString = "Bro";
+        int myInt = 50;
+        double myDouble = 1000;
 
-        System.out.println(x);
+        System.out.printf("%b", myBoolean);
+        System.out.printf("%c", myChar);
+        System.out.printf("%s",myString);
+        System.out.printf("%d",myInt);
+        System.out.printf("%f", myDouble);
 
-    }
 
-    static int add(int a, int b){
-        System.out.println("This is overloaded method #1");
-        return a+b;
-    }
-    static int add(int a, int b, int c ){
-        System.out.println("This is overloaded method #2");
-        return a+b+c;
-    }
-    static int add(int a, int b, int c, int d){
-        System.out.println("This is overloaded method #3");
-        return a+b+c+d;
-    }
-    static double add(double a, double b){
-        System.out.println("This is overloaded method #4");
-        return a+b;
-    }
-    static double add(double a, double b, double c ){
-        System.out.println("This is overloaded method #5");
-        return a+b+c;
-    }
-    static double add(double a, double b, double c, double d){
-        System.out.println("This is overloaded method #6");
-        return a+b+c+d;
+
     }
 }
 
