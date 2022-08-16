561b406c635efb0466a0e28fe415e55f5ffbbcf8
Oleksii Iashchuk <jawuk.alex@gmail.com>
2022-08-17 00:43:44 +0300

    global, local

--- src/Practice.java
+++ src/Practice.java
@@ -6,13 +6,10 @@ import java.util.Scanner;
 import java.util.*;
 
 public class Practice {
+
     public static void main(String args[]) {
 
-        Human human1 = new Human("Alex", 70, 34);
-        Human human2 = new Human("Mordi", 52, 47);
-
-        human2.eat();
-        human1.drink();
+        DiceRoller diceRoller = new DiceRoller();
 
 
     }
