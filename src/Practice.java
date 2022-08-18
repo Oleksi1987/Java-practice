e66a5e1447ca27195cffaf75720307f362e48058
Oleksii Iashchuk <jawuk.alex@gmail.com>
2022-08-19 01:44:39 +0300

    toString

--- src/Practice.java
+++ src/Practice.java
@@ -1,21 +1,14 @@
-import java.io.StringReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Locale;
-import java.util.Scanner;
-import java.util.*;
+
 
 public class Practice {
 
     public static void main(String args[]) {
-        Pizza pizza = new Pizza("thicc");
 
-        System.out.println("Here are the ingredients of pizza: ");
-        System.out.println(pizza.bread);
-        System.out.println(pizza.sauce);
-        System.out.println(pizza.cheese);
-        System.out.println(pizza.topping);
 
+        Car car = new Car();
+
+        //System.out.println(car.toString());
+        System.out.println(car);
 
     }
 }
