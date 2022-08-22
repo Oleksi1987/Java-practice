15c0dcf1acbe6ec97cf9d43585fff6f54f0bc48a
Oleksii <jawuk.alex@gmail.com>
2022-08-22 18:10:57 +0300

    static keyword

--- src/Practice.java
+++ src/Practice.java
@@ -2,13 +2,11 @@ public class Practice {
 
     public static void main(String args[]) {
 
-        Garage garage = new Garage();
+        Friend friend1 = new Friend("Bob");
+        Friend friend2 = new Friend("Vasul");
+        Friend friend3 = new Friend("Petro");
 
-        Car car1 = new Car("BMW");
-        Car car2 = new Car("Tesla");
-
-        garage.park(car1);
-        garage.park(car2);
+        Friend.displayFriends();
 
 
 
